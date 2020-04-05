a8723134c5187e7cd7789308a252797c699b9758
Emily Bowe <ebowe@tcd.ie>
2020-04-05 12:37:12 +0100

    Initial commit - Setup a class and test and make them more OO in next commits

---
+++ src/com/OODesign/Main.java
@@ -0,0 +1,8 @@
+package com.OODesign;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/OODesign/Money.java
@@ -0,0 +1,36 @@
+package com.OODesign;
+enum Currency {USD, EURO;
+public double conversionRateTo(Currency target){return 1.0;}
+}
+public class Money {
+    private double value;
+    private Currency currency;
+    public Money(double value, Currency currency) {
+        this.value = value;
+        this.currency = currency;
+    }
+    public double getValue() {
+        return value;
+    }
+    public void setValue(double value) {
+        this.value = value;
+    }
+    public Currency getCurrency() {return currency;}
+}
+class Test {
+    private static void dispenseFunds (Money amount) {}
+    public static void test() {
+        Money balance = new Money(1.0, Currency.USD);
+        Money request = new Money(3.0, Currency.EURO);
+
+        double normalizedBalance = balance.getValue() *
+                balance.getCurrency()
+                        .conversionRateTo(Currency.USD);
+        double normalizedRequest = request.getValue() *
+                request.getCurrency()
+                        .conversionRateTo(Currency.USD);
+        if(normalizedBalance > normalizedRequest) {
+            dispenseFunds(request);
+        }
+    }
+}
